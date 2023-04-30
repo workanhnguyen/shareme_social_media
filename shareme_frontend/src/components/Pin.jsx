ea1690f3b7595ec44455710f797e4d2668a7f425
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-04-30 18:31:07 +0700

    update

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -14,7 +14,7 @@ const Pin = ({ pin: { postedBy, imgUrl, _id, destination, save }}) => {
   const navigate = useNavigate();
 
   const user = fetchUser();
-  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.sub))?.length;
+  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user?.sub))?.length;
 
   const savePin = (id) => {
     if (!alreadySaved) {
@@ -23,10 +23,10 @@ const Pin = ({ pin: { postedBy, imgUrl, _id, destination, save }}) => {
         .setIfMissing({ save: [] })
         .insert('after', 'save[-1]', [{
           _key: uuidv4(),
-          userId: user.sub,
+          userId: user?.sub,
           postedBy: {
             _type: 'postedBy',
-            _ref: user.sub
+            _ref: user?.sub
           }
         }])
         .commit()
@@ -96,7 +96,7 @@ const Pin = ({ pin: { postedBy, imgUrl, _id, destination, save }}) => {
                   {destination.length > 15 ? `${destination.slice(0, 15)}...` : destination}
                 </a>
               )}
-              {postedBy?._id === user.sub && (
+              {postedBy?._id === user?.sub && (
                 <button
                   type='button'
                   onClick={(e) => {
