94de77daa0c923128c887120e47dad0d19262f50
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-04-30 18:35:03 +0700

    update

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -14,7 +14,7 @@ const Pin = ({ pin: { postedBy, imgUrl, _id, destination, save }}) => {
   const navigate = useNavigate();
 
   const user = fetchUser();
-  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user?.sub))?.length;
+  const alreadySaved = !!(save?.filter((item) => item?.postedBy?._id === user?.sub))?.length;
 
   const savePin = (id) => {
     if (!alreadySaved) {
